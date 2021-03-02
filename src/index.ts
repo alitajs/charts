fd114b7a5e097d6764fb19c887a3e0acb799da87
wulili-wll <996303052@qq.com>
2021-03-02 18:00:13 +0800

    feat: add -chart-SingleProgress

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 // 此文件在构建时会自动更新，请勿手动修改，详见 package.json 中的 entry script
 export { default as Donut } from './Donut';
 export { default as Column } from './Column';
+export { default as SingleProgress } from './SingleProgress';
